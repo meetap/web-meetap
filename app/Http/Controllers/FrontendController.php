bb443b435ed947d561fbbb7c7e6da97ba88322e0
Deni Supriyatna <denisupriyatna01@gmail.com>
2024-08-31 11:56:45 +0700

    add css and js

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,7 +11,7 @@ class FrontendController extends Controller
      */
     public function index()
     {
-        //
+        return view("frontend.app");
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index']);
 
 Auth::routes();
 
